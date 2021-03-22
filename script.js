071acffdb53eea13daa3b98f51b5fc95eeb78d20
interkoweb <interkoweb@gmail.com>
2021-03-22 21:54:02 +0400

    udpate

--- script.js
+++ script.js
@@ -27,33 +27,40 @@ for(let i=0; i<buttons.length; i++){
         event.preventDefault();
         if(buttons[i].classList ==''+i+''){
             if(i==0||i==1){
-							display_no(div1);
+				display_no(div1);
                 question.push(i);
                 display_yes(div2);
-							console.log("Вы нажали:", buttons[i]);
+				console.log("Вы нажали:", question[i]);
             }
             else if(i==2||i==3){
-							display_no(div2);
-							question.push(i);
-							display_yes(div3);
-							console.log("Вы нажали:", buttons[i]);						
+				display_no(div2);
+				question.push(i);
+				display_yes(div3);
+				console.log("Вы нажали:", question[i]);						
             }
-						else if(i==4||i==5){
-							display_no(div3);
-							display_yes(submit);
-							question.push(i);
-							console.log("Вы нажали:", buttons[i]);					
-						}
+			else if(i==4||i==5){
+				display_no(div3);
+				display_yes(submit);
+				question.push(i);
+				console.log("Вы нажали:", question[i]);					
+			}
 					
-						
+								
       }
 				
+	  submitForm.addEventListener('submit',function(event){
+		event.preventDefault();
+		display_no(submit);
+		let result = 'Вы нажали на блоки:<br>' + question[i] + ' <br><br>Вы просто гений. <br><br>Ваш код: <span class="sale_kod">' + sale_kod + '</span>';
+		document.getElementById("result").innerHTML = result;
+		console.log(question[i]);
 		
+	});
 		
 							
     });
 	 
-		
+	
 		
 }
 
@@ -63,13 +70,7 @@ function getRandomInt(min, max) {
 let sale_kod = getRandomInt(100000, 999999);
 
 
-submitForm.addEventListener('submit',function(event){
-	event.preventDefault();
-	display_no(submit);
-	let result = 'Вы нажали на блоки: '+question+'! <br><br>Вы просто гений. <br><br>Ваш код: <span class="sale_kod">' + sale_kod + '</span>';
-	document.getElementById("result").innerHTML = result;
-	
-});
+
 
 
 
