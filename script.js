dcf1001a061f7b3f34879108dada95b7a60d5162
Interko <interkoweb@gmail.com>
2021-03-22 21:09:07 +0300

    UPDATE

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let buttons=document.getElementsByTagName('button');
 let question=[
-	vizitki = 'Визитки',
-	bukleti = 'Буклеты',
-	
-
+	'Визитки',
+	'Буклеты',
+	'Юрлицо',
+	'ИП', 
+	'Автозавод',
+	'Нижегородский'
 ];
 let div1=document.querySelector('#div1');
 let div2=document.querySelector('#div2');
@@ -12,11 +14,11 @@ let div4=document.querySelector('#div4');
 let submit=document.querySelector('#submit');
 let submitForm=document.querySelector('#submitForm');
 
-function addHidden(event){
-    event.classList.add('hidden');
+function display_no(event){
+    event.classList.add('display-no');
 	}
-function removeHidden(event){
-    event.classList.remove('hidden');
+function display_yes(event){
+    event.classList.remove('display-no');
 	}
 
 for(let i=0; i<buttons.length; i++){
@@ -25,35 +27,33 @@ for(let i=0; i<buttons.length; i++){
         event.preventDefault();
         if(buttons[i].classList ==''+i+''){
             if(i==0||i==1){
-                addHidden(div1);
+							display_no(div1);
                 question.push(i);
-                removeHidden(div2);
-								
+                display_yes(div2);
+							console.log("Вы нажали:", buttons[i]);
             }
             else if(i==2||i==3){
-							addHidden(div2);
+							display_no(div2);
 							question.push(i);
-							removeHidden(div3);
-														
+							display_yes(div3);
+							console.log("Вы нажали:", buttons[i]);						
             }
 						else if(i==4||i==5){
-							addHidden(div3);
-							removeHidden(submit);
+							display_no(div3);
+							display_yes(submit);
 							question.push(i);
-						console.log(question);
-						console.log(question.length);
-							
+							console.log("Вы нажали:", buttons[i]);					
 						}
 					
-			
+						
       }
-				console.log(question);
-
+				
+		
 		
 							
     });
 	 
-	
+		
 		
 }
 
@@ -65,16 +65,14 @@ let sale_kod = getRandomInt(100000, 999999);
 
 submitForm.addEventListener('submit',function(event){
 	event.preventDefault();
-	addHidden(submit);
-	let result = 'Вы нажали на блоки: '+question[0]+' , '+question[1]+' и '+question[2]+'! <br><br>Вы просто гений. <br><br>Ваш код для скидки <span class="sale_kod">' + sale_kod + '</span>';
+	display_no(submit);
+	let result = 'Вы нажали на блоки: '+question+'! <br><br>Вы просто гений. <br><br>Ваш код: <span class="sale_kod">' + sale_kod + '</span>';
 	document.getElementById("result").innerHTML = result;
 	
-	console.log("Вы нажали на "+question[0]+" и "+question[1]+"кнопки");
-
 });
 
 
 
 
-	
+
 
